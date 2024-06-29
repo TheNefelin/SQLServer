53d0439c4053fe9a453b3618268eef0ec24697e0
Francisco Carmona <flcarmonac@yahoo.com>
2024-06-29 19:29:21 -0400

    Auth v3

---
+++ Auth_v3.sql
@@ -0,0 +1,144 @@
+CREATE DATABASE db_auth
+
+USE db_auth
+
+-- Tablas -------------------------------------------------------
+-- --------------------------------------------------------------
+CREATE TABLE Perfiles (
+	Id INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(50) NOT NULL,
+	UNIQUE(Nombre),
+)
+GO
+
+CREATE TABLE Usuarios (
+	Id VARCHAR(256) PRIMARY KEY,
+	Email VARCHAR(100) NOT NULL,
+	Usuario VARCHAR(100) NOT NULL,
+	AuthHash VARBINARY(64) NOT NULL,
+	AuthSalt VARBINARY(16) NOT NULL,
+	SessionCode VARCHAR(256) ,
+	Id_Perfil INT NOT NULL
+	UNIQUE(Usuario),
+	UNIQUE(Email),
+	FOREIGN KEY (Id_Perfil) REFERENCES Perfiles(Id)
+)
+GO
+
+DROP TABLE Perfiles
+DROP TABLE Usuarios
+
+-- Data ---------------------------------------------------------
+-- --------------------------------------------------------------
+SET IDENTITY_INSERT Perfiles ON
+GO
+INSERT INTO Perfiles
+	(Id, Nombre)
+VALUES
+	(1, 'Admin'),
+	(2, 'Usuario')
+SET IDENTITY_INSERT Perfiles OFF
+GO
+
+-- Stored Procedure ---------------------------------------------
+-- --------------------------------------------------------------
+CREATE PROCEDURE Auth_Register
+	@Id VARCHAR(256),
+	@Email VARCHAR(100),
+    @Usuario NVARCHAR(100),
+    @Clave NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+	
+    DECLARE @Salt VARBINARY(16)
+    SET @Salt = CRYPT_GEN_RANDOM(16)
+
+    DECLARE @Hash VARBINARY(64)
+    SET @Hash = HASHBYTES('SHA2_256', @Clave + CAST(@Salt AS NVARCHAR(32)))
+
+	IF EXISTS (SELECT Id FROM Usuarios WHERE Email = @Usuario OR Usuario = @Usuario)
+		BEGIN
+			SELECT 400 AS StatusCode, 0 AS Id, 'El Usuario ya Existe' AS Msge
+			RETURN
+		END
+
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+		INSERT INTO Usuarios 
+			(Id, Usuario, Email, AuthHash, AuthSalt, Id_Perfil)
+		VALUES 
+			(@Id, @usuario, @usuario, @hash, @salt, 2)
+			
+		COMMIT TRANSACTION
+
+		SELECT 201 AS StatusCode, 0 AS Id, 'Usuario Registrado Correctamente' AS Msge
+    END TRY
+    BEGIN CATCH
+		ROLLBACK TRANSACTION
+
+		SELECT 500 AS StatusCode, 0 AS Id, 'Error al Guardado los Datos (Auth_Register)' AS Msge 
+    END CATCH
+END
+GO
+
+CREATE PROCEDURE Auth_Login
+    @Usuario NVARCHAR(100),
+    @Clave NVARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+    DECLARE @Hash VARBINARY(64)
+    DECLARE @Salt VARBINARY(16)
+    DECLARE @HashProvidedPassword VARBINARY(64)
+
+    SELECT 
+		@Hash = AuthHash, @Salt = AuthSalt
+    FROM Usuarios 
+	WHERE 
+		Usuario = @Usuario
+
+    SET @hashProvidedPassword = HASHBYTES('SHA2_256', @Clave + CAST(@Salt AS NVARCHAR(32)))
+
+    IF @Hash <> @hashProvidedPassword
+		BEGIN
+			SELECT 400 AS StatusCode, 0 AS Id, 'Usuario o Contraseña Incorrecta' AS Msge
+			RETURN
+		END
+
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+		UPDATE Usuarios SET
+			SessionCode = NEWID()
+		WHERE 
+			Usuario = @Usuario
+		
+		COMMIT TRANSACTION
+
+		SELECT 201 AS StatusCode, 0 AS Id, 'Inicio de Sesión Exitoso.' AS Msge
+    END TRY
+    BEGIN CATCH
+		ROLLBACK TRANSACTION
+
+		SELECT 500 AS StatusCode, 0 AS Id, 'Error al Iniciar Sesion (Auth_Login)' AS Msge 
+    END CATCH
+
+END
+GO
+
+DROP PROCEDURE Auth_Register
+DROP PROCEDURE Auth_Login
+
+-- Query --------------------------------------------------------
+-- --------------------------------------------------------------
+SELECT * FROM Perfiles
+SELECT * FROM Usuarios
+
+EXECUTE Auth_Register 'ABCD123', 'prueba@db.cl', 'prueba@db.cl', '123456' 
+EXECUTE Auth_Login 'prueba@db.cl', '123456' 
+
+-- --------------------------------------------------------------
+-- --------------------------------------------------------------
